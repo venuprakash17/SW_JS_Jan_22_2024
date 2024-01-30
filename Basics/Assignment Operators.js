409eedbb8d14235bfb181b5cd77955b622c28643
THIPPI REDDY VENU PRAKASH REDDY <49930985+venuprakash17@users.noreply.github.com>
2024-01-30 08:14:22 +0530

    Create Assignment Operators.js

---
+++ Basics/Assignment Operators.js
@@ -0,0 +1,25 @@
+let a = 10;
+// Multiplication
+console.log('a value before Multiplication "*": '+ a) 
+a *= 2
+console.log('a value after Multiplication "*": '+ a) // a = 20 
+
+let b = 5;
+console.log('b value before Division "/": '+ b) 
+// Division
+b /= 5
+console.log('b value after Division "/": '+ b) // b = 1;
+
+let c = 21;
+console.log('c value before Modulus "%": '+c)
+// Modulus % 
+
+c %= 5;
+console.log('c value after Modulus "%": '+c);
+
+let d = 2;
+console.log('d value before Exponent "**": '+d)
+// Exponent
+
+d **=3
+console.log('d value after Exponent "**": '+d);
