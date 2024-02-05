d789625539ebf384b55447ed6bc9818e71feceb3
THIPPI REDDY VENU PRAKASH REDDY <49930985+venuprakash17@users.noreply.github.com>
2024-02-05 23:05:49 +0530

    Create Data_types.js

---
+++ Week_1/Day_2/Data_types.js
@@ -0,0 +1,73 @@
+//  This file is all about data types.
+
+// Null and Undefined
+
+/*
+let a = 10;
+console.log('a: '+a)
+
+let b = null;
+console.log("Value of b: "+b)
+console.log(typeof b)
+
+let c;
+console.log('value of c: '+c)
+console.log(typeof c)
+
+*/
+
+// Numbers:
+
+/*
+let n = 10;
+let n_f = 10.212
+console.log('n: '+n)
+console.log("Type of n: "+typeof n)
+console.log('n_f: '+n_f)
+console.log("Type of n_f: "+typeof n_f)
+*/
+
+//  Strings:
+
+/* 
+
+let str = "Script Wave"
+console.log('Value of STR: ' + str);
+console.log('Type  of STR: ' + typeof str);
+let str_n = "12345@#$@!#"
+console.log('Type  of STR_N: ' + typeof str_n);
+
+let n_1 = "10"
+let n_2 = "20"
+
+let n_sum = n_1 + n_2;
+
+console.log('Sum of n1 and n2: '+n_sum);
+
+
+
+let concat = "Script" + "_" + "Wave"
+
+console.log('concat: ', concat)
+
+let n_1a = 10;
+let n_2a = 20;
+console.log("Sum of n_1a + n_2a", n_1a + n_2a);
+
+// String Letrals
+
+let name_new = "ScriptWave";
+let num = 10
+let lst = [10,20]
+let greet = `Hello!, How are you ${name_new}, number is ${num}, the list [${lst}]`;
+
+console.log("Greet ",greet)
+*/
+
+//  Boolean:
+let x = 10;
+let y = 20;
+console.log(x<y);
+console.log(x>y);
+
+
