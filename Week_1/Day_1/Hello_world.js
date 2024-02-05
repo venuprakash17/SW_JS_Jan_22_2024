1c307423a39d999452b8a89c085ac5b2a7c15706
THIPPI REDDY VENU PRAKASH REDDY <49930985+venuprakash17@users.noreply.github.com>
2024-02-05 11:49:35 +0530

    Update Hello_world.js

--- Week_1/Day_1/Hello_world.js
+++ Week_1/Day_1/Hello_world.js
@@ -1,4 +1,11 @@
 // Video Link  https://us06web.zoom.us/clips/share/U_yOc7eRH78pNbFwbBrTlV7xX95O17Bill-KeJHxfMDU0LJdkDps-TrhitmJ6quyGPOQRPKfgTRFB-qYFiZUyrD7.9063keX_eENIuMMA
 // Code:
-
 console.log('Hello World')
+
+
+// Video Link: https://www.youtube.com/watch?v=ALcZH-r6KVI
+Code: 
+// Variables and values.
+let a = "Value"
+
+console.log("a: "+a);
