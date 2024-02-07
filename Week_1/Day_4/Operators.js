9bf754402d69b9873c182a998b692edfb8feb72d
THIPPI REDDY VENU PRAKASH REDDY <49930985+venuprakash17@users.noreply.github.com>
2024-02-07 19:05:18 +0530

    Add files via upload

---
+++ Week_1/Day_4/Operators.js
@@ -0,0 +1,120 @@
+// **** operators ****
+
+
+// Arithematic operators ****
+
+/*
+
+// Addition
+let a_add = 10
+let b_add = 20
+
+console.log('Sum of 10 and 20', a_add + b_add)
+
+// Subtraction
+let a_sub = 20
+let b_sub = 30
+console.log('Subtraction of 20 and 30', a_sub - b_sub)
+
+// Multiplication
+
+let a_mult = 10
+let b_mult = 2
+console.log('Multiplication of 10 and 2', a_mult * b_mult)
+
+// Division
+let a_div = 10
+let b_div = 2
+console.log('Division of 10 and 2', a_div / b_div)
+
+// Modulus 
+
+let a_mod = 11
+let b_mod = 2
+console.log('MOdulus of 11 and 2', a_mod % b_mod)
+
+// Increment and decrement operators:
+
+let a = 5;
+let b = a++; // Postfix increment: b will be assigned the value of 'a' (5), then 'a' will be incremented by 1
+console.log("a:", a); // Output: 6
+console.log("b:", b); // Output: 5
+
+let c = 5;
+let d = ++c; // Prefix increment: 'c' will be incremented by 1 first, then 'd' will be assigned the new value of 'c'
+console.log("c:", c); // Output: 6
+console.log("d:", d); // Output: 6
+
+// decrement 
+let a_dec = 5;
+let b_dec = a_dec--; // Postfix increment: b will be assigned the value of 'a' (5), then 'a' will be incremented by 1
+console.log("a_dec:", a_dec); // Output: 4
+console.log("b_dec:", b_dec); // Output: 5
+
+let c_dec = 5;
+let d_dec = --c_dec; // Prefix increment: 'c' will be incremented by 1 first, then 'd' will be assigned the new value of 'c'
+console.log("c_dec:", c_dec); // Output: 4
+console.log("d_dec:", d_dec); // Output: 4
+
+*/
+// Assignment operators:
+/*
+let a_assign = 10;
+let b_assign = a_assign
+console.log(b_assign)
+console.log(a_assign)
+*/
+
+/*
+// Addition Assignment:
+
+let a_add_assign = 10;
+a_add_assign += 10 // a_add_assign = a_add_assign + 10
+console.log("a_add_assign: ", a_add_assign)
+
+
+// Multiplication Assignment:
+
+let a_mult_assign = 10;
+a_mult_assign *= 10 // a_add_assign = a_add_assign * 10
+console.log("a_mult_assign: ", a_mult_assign)
+
+// Exponential Assignment:
+
+let a_exp_assign = 10;
+a_exp_assign **= 3 // a_add_assign = a_add_assign ** 2
+console.log("a_exp_assign: ", a_exp_assign)
+
+*/
+
+
+
+
+
+// *** Comparision Operator:
+
+/*
+console.log('10 == 10', 10 == 10); // Equality 
+console.log('10 === 10', 10 === 10); // Strict equality 
+console.log("20 == '20'", 20 == '20');
+console.log("20 === '20'", 20 === '20');
+
+console.log("10 != '10'", 10 != '10') // Inequality 
+console.log("10 !== '10'", 10 !== '10') // Strict inequality 
+
+console.log('10 > 11', 10 > 11) // Greater
+console.log('10 < 11', 10 < 11) // Less than  
+
+
+
+console.log('10 >= 10', 10 >= 10) // Greater
+console.log('10 <= 11', 10 <= 11) // Less than 
+
+*/
+
+
+// ****** Logiocal Operators ***********
+
+console.log(10 == 10 && 10 > 9);
+console.log(10 == 10 && 10 > 9);
+
