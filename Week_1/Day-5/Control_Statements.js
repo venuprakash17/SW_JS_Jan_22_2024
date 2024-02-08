ff5ec7023752e22e6629e7acb151427d704c0e02
THIPPI REDDY VENU PRAKASH REDDY <49930985+venuprakash17@users.noreply.github.com>
2024-02-08 09:17:08 +0530

    Add files via upload

---
+++ Week_1/Day-5/Control_Statements.js
@@ -0,0 +1,74 @@
+//  Video Link:
+
+// Code:
+/*
+
+// ********** IF StateMent *********
+let a = 11;
+let b = 20;
+if(a==10){
+    console.log('A is 10');
+}
+console.log('Value of b: ', b )
+
+*/
+
+/*
+
+// ********** IF Else StateMent *********
+let age = 17;
+if(age>=18){
+    console.log('The person is elgible for vote')
+}else{
+    console.log('The person is not elgible for vote')
+}
+*/
+
+/*
+let num = 13;
+if(num % 2 == 0){
+    console.log('The given number is even')
+}else{
+    console.log('ODD NUMBER')
+}
+*/
+
+/*
+
+
+// ********** ELSE IF StateMent *********
+// B-tech, Degree, Pharma.
+
+let selection = 'Inter'
+
+if(selection == 'B-tech'){
+    console.log('ECE, CSE, EEE')
+}else if(selection == 'Degree'){
+    console.log('B.com, BA, BZC')
+}else if(selection == 'Pharma'){
+    console.log('A-ph, B-ph, D-ph')
+}else{
+    console.log('please do a vaild selection')
+}
+*/
+
+/*
+// ***** ` Ternary Operator` *******
+
+let a_age = 17;
+
+let can_vote = (a_age >= 18) ? "Elgible": "Not Elgible"
+
+let can_vote_1;
+
+if(a_age >=18){
+    can_vote_1 = 'Eligible'
+}else{
+    can_vote_1 = 'Not Eligible'
+}
+
+
+console.log('can_vote_1: ', can_vote_1)
+console.log('can_vote: ', can_vote)
+*/
+
