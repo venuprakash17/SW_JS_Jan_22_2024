20aa9acfd49dc675f17cfcd88cda6343484e9459
THIPPI REDDY VENU PRAKASH REDDY <49930985+venuprakash17@users.noreply.github.com>
2024-02-14 10:37:20 +0530

    Add files via upload

---
+++ Week_2/Day_3/Functions-Return_statments_Arrow_functions.js
@@ -0,0 +1,43 @@
+'use strict';
+/*
+// Traditional way of function decleration.
+let number = Number(prompt('Enter the number that you want to find even / odd'))
+let even_odd = function (n) {
+    if (n % 2 == 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
+}
+
+console.log(`(Traditional:)The given number ${number} is ${even_odd(number)}`);
+
+// Arrow function:
+
+const Arrow1 = (n) => {
+    if (n % 2 == 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
+}
+
+console.log(`(Arrow:)The given number ${number} is ${Arrow1(number)}`);
+
+
+const arrow_single_line = (a, b) => a * b;
+
+console.log('arrow_single_line: ', arrow_single_line(10, 20))
+
+function arrow_single_trd(a, b) {
+    return a * b
+}
+*/
+
+// Write a program to simulate calculator working.
+
+
+// let ask = prompt('Enter the first value and second value with a comma')
+// // let b = Number(prompt('Enter the Second value'))
+// let opp = prompt('Enter the operation: + - addition; - - for subtraction; / for division; * for multiplication; % - for reminder; ** for exponential')
+
