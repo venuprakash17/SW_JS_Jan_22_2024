0204ba052c28691921c37c9ee9f3fef8c8c8e0c5
THIPPI REDDY VENU PRAKASH REDDY <49930985+venuprakash17@users.noreply.github.com>
2024-02-19 13:42:17 +0530

    Add files via upload

---
+++ Week_3/Day_1/Array for each.js
@@ -0,0 +1,40 @@
+//  Arrays in JS
+// Video = https://youtu.be/KICOJdxVZUw
+/*
+"use strict";
+const arr = ['Banana', 'Mango', 'apple']
+
+console.log(typeof arr)
+console.log('Array before push', arr)
+alert(arr[2])
+// push
+arr.push('Tomato')
+console.log('Array after push', arr)
+
+arr.push('grapes')
+console.log('Array after 2nd push', arr)
+
+
+//  pop 
+
+console.log('poped: ', arr.pop())
+console.log('After pop arr ', arr)
+
+*/
+
+let arr1 = [2, 3, 3, 4, 5, 5, 5, 4, 2, 3, 4, 5, 6, 6, 7, 7, 8, 8]
+let sum = 0
+// for (let i = 0; i < arr1.length; i++) {
+//     console.log(arr1[i]);
+//     sum = sum + arr1[i];
+// }
+
+
+
+// arr1.forEach((a) => {
+//     console.log(a)
+//     sum = sum + a
+// })
+
+
+// alert(sum)
